349bdf5af6deb95b3cfa7f2cbb6f759295ee465a
Cameron Lakenen <lakenen@box.com>
2015-01-05 10:12:57 -0800

    Add recursive serialization support (fix #1)

--- index.js
+++ index.js
@@ -4,10 +4,25 @@ var querystring = require('querystring')
 
 module.exports = function appendQuery(uri, q) {
   var parts = url.parse(uri, true)
-    , parsedQuery = typeof q === 'string' ? querystring.parse(q) : q
+    , parsedQuery = extend(true, {}, parts.query, typeof q === 'string' ? querystring.parse(q) : q)
 
-  extend(parts.query, parsedQuery)
-  // it's necessary to delete parts.search so parts.query will be used
-  delete parts.search
+  parts.search = '?' + serialize(parsedQuery)
   return url.format(parts)
+}
+
+// serialize an object recursively
+function serialize(obj, prefix) {
+  var str = []
+
+  Object.keys(obj).forEach(function (prop) {
+    var val = obj[prop]
+      , key = prefix ? prefix + '[' + prop + ']' : prop
+      , query = typeof val === 'object' ?
+        serialize(val, key) :
+        encodeURIComponent(key) + '=' + encodeURIComponent(val)
+
+    str.push(query)
+  })
+
+  return str.join('&')
 }
